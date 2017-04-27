2d28d62933caf3c1b0abe972135e5fab31813be2
Oleg <iamolegkovalov@gmail.com>
2017-04-27 19:54:30 +0200

    new(multierr): Multierr implementation

---
+++ multerr/multierr.go
@@ -0,0 +1,34 @@
+package multierr
+
+import "bytes"
+
+// Multierr struct allows to have multiple errors together
+type Multierr struct {
+	errors []error
+}
+
+// NewMultierr creates a new pointer to Multierr
+func NewMultierr() *Multierr {
+	return &Multierr{}
+}
+
+// Add adds error to the collection of errors
+func (m *Multierr) Add(err error) {
+	m.errors = append(m.errors, err)
+}
+
+// Len return a number of errors in the collection
+func (m *Multierr) Len() int {
+	return len(m.errors)
+}
+
+// Error error interface implementation
+func (m *Multierr) Error() string {
+	var buffer bytes.Buffer
+
+	for _, err := range m.errors {
+		buffer.WriteString(err.Error())
+		buffer.WriteString("\n")
+	}
+	return buffer.String()
+}
---
+++ multerr/multierr_test.go
@@ -0,0 +1,36 @@
+package multierr
+
+import (
+	"errors"
+	"testing"
+)
+
+func TestNewMultierr(t *testing.T) {
+	multerr := NewMultierr()
+	if multerr == nil {
+		t.Error("cannot instantiate Multierr")
+	}
+
+	if multerr.Len() != 0 {
+		t.Errorf("incorrect number of errors, expected %v got %v", 0, multerr.Len())
+	}
+
+	multerr.Add(errors.New("first error"))
+
+	if multerr.Len() != 1 {
+		t.Errorf("incorrect number of errors, expected %v got %v", 1, multerr.Len())
+	}
+
+	multerr.Add(errors.New("second error"))
+
+	if multerr.Len() != 2 {
+		t.Errorf("incorrect number of errors, expected %v got %v", 2, multerr.Len())
+	}
+
+	expected := "first error\nsecond error\n"
+	res := multerr.Error()
+
+	if res != expected {
+		t.Errorf("incorrect error value, expected \n`%v` got \n`%v`", expected, res)
+	}
+}
