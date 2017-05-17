30c838eeb6524511577e5c8d60e3b29ddc17796e
Oleg <iamolegkovalov@gmail.com>
2017-05-17 07:40:43 +0200

    optional: Optional monad implementation

---
+++ optional/optional.go
@@ -0,0 +1,54 @@
+package optional
+
+// Optional is a monad Maybe
+type Optional interface {
+	Apply(func(interface{}) Optional) Optional
+	HasValue() bool
+	Or(interface{}) interface{}
+}
+
+// Wrap returns a value wrapped to monad
+func Wrap(value interface{}) Optional {
+	return &Just{value}
+}
+
+// None is a non-value way
+type None struct{}
+
+// Just is a value way
+type Just struct {
+	value interface{}
+}
+
+// Apply on None do nothing
+func (n *None) Apply(f func(interface{}) Optional) Optional {
+	return n
+}
+
+// HasValue for None is alwaya false
+func (n *None) HasValue() bool {
+	return false
+}
+
+// Or for None always return a param
+func (n *None) Or(value interface{}) interface{} {
+	return value
+}
+
+// Apply executes f on monad value
+func (j *Just) Apply(f func(interface{}) Optional) Optional {
+	return f(j.value)
+}
+
+// HasValue is true for non-nil value
+func (j *Just) HasValue() bool {
+	return j.value != nil
+}
+
+// Or returns a param if monad value is nil
+func (j *Just) Or(value interface{}) interface{} {
+	if j.value == nil {
+		return value
+	}
+	return j.value
+}
---
+++ optional/optional_test.go
@@ -0,0 +1,38 @@
+package optional
+
+import "testing"
+
+func TestOptional(t *testing.T) {
+	f := func(v interface{}) Optional {
+		return &Just{v}
+	}
+	g := func(v interface{}) Optional {
+		return &None{}
+	}
+	h := func(v interface{}) Optional {
+		return &Just{nil}
+	}
+	o := Wrap(1)
+
+	if !o.HasValue() {
+		t.Error("must have value")
+	}
+
+	if value := o.Apply(f).Or(2); value != 1 {
+		t.Errorf("want 1, got %v", value)
+	}
+
+	o = g(2)
+	if o.HasValue() {
+		t.Error("must not have value")
+	}
+
+	if value := o.Apply(g).Or(2); value != 2 {
+		t.Errorf("want 2, got %v", value)
+	}
+
+	o = h(3)
+	if value := o.Or(3); value != 3 {
+		t.Errorf("want 3, got %v", value)
+	}
+}
